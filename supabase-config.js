0b3a1652eae44694eae4b3357452332f67c928fa
Alejo Paz <alejo.pazdiaz82@gmail.com>
2026-04-05 18:53:21 -0600

    Fix: Make supabase client global

--- supabase-config.js
+++ supabase-config.js
@@ -5,5 +5,5 @@
 const SUPABASE_URL = 'https://mrpchslrqjjlieqsgeca.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ycGNoc2xycWpqbGllcXNnZWNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0MzExNzcsImV4cCI6MjA5MTAwNzE3N30.ZFhS0Ijliy6x2zYOv5OMKzN698_AvjkgsoVxmYd_Dv8';
 
-// Inicializar el cliente de Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Inicializar el cliente de Supabase (sin const para que sea global)
+supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
